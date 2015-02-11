911efaee52671e5bc98e31a722de9a9bf3764ee3
Joseph Arhar <josepharhar@gmail.com>
2015-02-11 14:52:23 -0800

    reformatted

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        HangmanFrame gui = new HangmanFrame();
 
-//		HangmanFrame gui = new HangmanFrame(theWord);
-		HangmanFrame gui = new HangmanFrame();
-		
-		gui.setSize(HangmanFrame.SCREEN_WIDTH, HangmanFrame.SCREEN_HEIGHT);
-		gui.setLocationRelativeTo(null);
-		gui.setVisible(true);
-	}
+        gui.setSize(HangmanFrame.SCREEN_WIDTH, HangmanFrame.SCREEN_HEIGHT);
+        gui.setLocationRelativeTo(null);
+        
+        gui.setVisible(true);
+    }
 }
--- src/MainMenuPanel.java
+++ src/MainMenuPanel.java
@@ -6,51 +6,53 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
 public class MainMenuPanel extends JPanel {
-	
-	private JButton startOnePlayer;
-	private JButton startTwoPlayer;
-	
-	private HangmanFrame mainFrame;
-	
-	public MainMenuPanel(HangmanFrame newMainFrame) {
-		mainFrame = newMainFrame;
-		
-		startOnePlayer = new JButton("One Player");
-		startTwoPlayer = new JButton("Two Player");
 
-		startOnePlayer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//get random word, set it to the secret word, and start the game
-				mainFrame.start(getRandomWord());
-			}
-		});
+    private JButton startOnePlayer;
+    private JButton startTwoPlayer;
 
-		startTwoPlayer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//ask for a word from the user, and start the game
-				String secretWord = JOptionPane.showInputDialog("Enter the secret word: ");
-				mainFrame.start(secretWord);
-			}
-		});
-		
-		setLayout(new BorderLayout());
-		
-		add(startOnePlayer, BorderLayout.NORTH);
-		add(startTwoPlayer, BorderLayout.SOUTH);
-	}
-	
-	private String getRandomWord() {
+    private HangmanFrame mainFrame;
 
-		String[] presetWords = {"bird", "cat", "dog", "computer", "Java", "programming", "lollapalooza"};
-		
-		//randomly select a word from the array
-		String secretWord = presetWords[(int) (Math.random() * presetWords.length)];
-		
-		//make the word
-		Word theWord = new Word("bird");
-		
-		return "bird";
-	}
+    public MainMenuPanel(HangmanFrame newMainFrame) {
+        mainFrame = newMainFrame;
+
+        startOnePlayer = new JButton("One Player");
+        startTwoPlayer = new JButton("Two Player");
+
+        startOnePlayer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // get random word, set it to the secret word, and start the
+                // game
+                mainFrame.start(getRandomWord());
+            }
+        });
+
+        startTwoPlayer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // ask for a word from the user, and start the game
+                String secretWord = JOptionPane
+                        .showInputDialog("Enter the secret word: ");
+                mainFrame.start(secretWord);
+            }
+        });
+
+        setLayout(new BorderLayout());
+
+        add(startOnePlayer, BorderLayout.NORTH);
+        add(startTwoPlayer, BorderLayout.SOUTH);
+    }
+
+    private String getRandomWord() {
+
+        String[] presetWords = { "bird", "cat", "dog", "computer", "Java",
+                "programming", "lollapalooza" };
+
+        // randomly select a word from the array
+        String secretWord = presetWords[(int) (Math.random() * presetWords.length)];
+
+        // make the word
+        Word theWord = new Word("bird");
+
+        return "bird";
+    }
 }
